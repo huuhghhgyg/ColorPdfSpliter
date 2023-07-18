fdb311d4e19bba43f1b7ee1397385d76939cab01
huuhghhgyg <huuhghhgyg@outlook.com>
2023-07-19 01:03:47 +0800

    初始上传

---
+++ ColorPdfSpliter.py
@@ -0,0 +1,98 @@
+# 导入需要的模块
+import os
+
+import fitz  # 用于处理pdf文件
+import numpy as np  # 用于处理数组
+import glob  # 用于获取当前目录文件
+
+# 参数设置
+RGBDiff = 10  # RGB颜色总差异之和
+ExportDir = './export'
+
+# 定义一个函数，判断一个页面是否为彩色页面
+def is_color_page(page):
+    # 获取页面的像素矩阵
+    pix = page.get_pixmap()
+    # 将像素矩阵转换为numpy数组
+    arr = np.frombuffer(pix.samples, dtype=np.uint8).reshape(pix.h, pix.w, pix.n)
+    # 如果像素矩阵的通道数大于1，说明有颜色信息
+    if pix.n > 1:
+        # 计算每个像素的灰度值
+        gray = np.dot(arr[..., :3], [0.299, 0.587, 0.114])
+        # 计算每个像素的颜色差异值
+        diff = np.abs(arr[..., :3] - gray[..., None]).sum(axis=2)
+        # 如果颜色差异值的平均值大于某个阈值，说明页面为彩色页面
+        if np.any(diff > RGBDiff):
+            return True
+
+    # 否则，页面为非彩色页面
+    return False
+
+
+def SplitPDF(file, exportdir=''):
+    # 文件名设置
+    filename = {
+        'input': file,
+        'graypages': file.strip('.pdf') + '_黑白.pdf',
+        'colorpages': file.strip('.pdf') + '_彩色.pdf'
+    }
+
+    # 打开一个pdf文件
+    doc = fitz.open(file)
+    # 创建两个空的pdf文件，用于保存彩色页面和非彩色页面
+    color_doc = fitz.open()
+    gray_doc = fitz.open()
+
+    count = {'page': 0, 'gray': 0, 'color': 0}
+    # 遍历原pdf文件中的每个页面
+    for page in doc:
+        count['page'] = count['page'] + 1
+        print("检测页面：", count['page'],'/',len(doc))
+        # 判断页面是否为彩色页面
+        if is_color_page(page):
+            # 如果是，将页面添加到彩色pdf文件中
+            color_doc.insert_pdf(doc, from_page=page.number, to_page=page.number)
+            count['color'] = count['color'] + 1
+        else:
+            # 如果不是，将页面添加到非彩色pdf文件中
+            gray_doc.insert_pdf(doc, from_page=page.number, to_page=page.number)
+            count['gray'] = count['gray'] + 1
+
+    # 保存两个pdf文件
+    # 保存灰色页面
+    if count['gray']>0:
+        print("正在保存灰色页面(共", count['gray'], "页):", filename['graypages'])
+        gray_doc.save(exportdir + filename['graypages'])
+    else:
+        print('文件',filename['input'],'分割后不存在灰色页面，不保存文件')
+
+    # 保存彩色页面
+    if count['color']>0:
+        print("正在保存彩色页面(共", count['color'], "页):", filename['colorpages'])
+        color_doc.save(exportdir + filename['colorpages'])
+    else:
+        print('文件', filename['input'], '分割后不存在彩色页面，不保存文件')
+
+
+# 总流程
+pdf_list = glob.glob('*.pdf')
+if len(pdf_list) == 0:  # 没有pdf文件
+    print('未检测到当前目录中存在pdf文件')
+    os.system('pause')
+    exit()
+
+if len(pdf_list) > 1:
+    # 判断批量输出目录是否存在
+    if not os.path.exists(ExportDir):
+        os.mkdir(ExportDir)
+
+    for i in range(len(pdf_list)):
+        print('[', i + 1, '/', len(pdf_list), '] 正在处理:', pdf_list[i])
+        SplitPDF(pdf_list[i],'./export/')
+        print('\n')
+
+    print('多项文件已保存至',ExportDir)
+elif len(pdf_list) == 1:
+    SplitPDF(pdf_list[0])
+
+print("已完成")
---
+++ initialize.bat
@@ -0,0 +1,5 @@
+pip install PyMuPDF numpy
+
+@REM 修复
+@REM pip uninstall fitz
+@REM pip install --upgrade --force-reinstall pymupdf
